60a093f0f4ca8f17d86579b23b1905bf245aaaa5
NivedithaG <niveditha.gunda@gmail.com>
2025-10-27 16:40:00 +0530

    improved code logic

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int[] res = LC1.twoSum(new int[]{2,7,11,15}, 9);
         for (int n : res) {
-        System.out.println(n);
+        System.out.print(n + " ");
         }
     }
     
