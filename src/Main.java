a7b264e23a2ad129f18f5606ce9b0002ee962184
NivedithaG <niveditha.gunda@gmail.com>
2025-10-27 13:20:18 +0530

    Initial commit - setup LeetCode practice project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import Arrays.LC1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] res = LC1.twoSum(new int[]{2,7,11,15}, 9);
+        for (int n : res) {
+        System.out.println(n);
+        }
+    }
+    
+}
